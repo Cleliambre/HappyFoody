4d6be97f9e59dfc1ba0d60260c5818372492b0f4
Cleliambre <168779349+Cleliambre@users.noreply.github.com>
2025-10-02 20:13:46 +0200

    added Quantite service & controller

--- src/main/java/com/example/happy_foody/repository/QuantiteRepository.java
+++ src/main/java/com/example/happy_foody/repository/QuantiteRepository.java
@@ -1,9 +1,10 @@
 package com.example.happy_foody.repository;
 
 import com.example.happy_foody.model.Quantite;
+import com.example.happy_foody.model.QuantiteId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuantiteRepository  extends JpaRepository<Quantite, Long> {
+public interface QuantiteRepository  extends JpaRepository<Quantite, QuantiteId> {
 }
