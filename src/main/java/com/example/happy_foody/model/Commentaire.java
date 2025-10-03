e32979589551c34e77698b0d3c5ae3cb585e1491
abellik <admblk04@gmail.Com>
2025-10-03 11:07:21 +0200

    created table

---
+++ src/main/java/com/example/happy_foody/model/Commentaire.java
@@ -0,0 +1,45 @@
+package com.example.happy_foody.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="commentaire")
+public class Commentaire {
+    private long idCommentaire;
+    private Commentaire CommRepondu;
+    private Compte auteur;
+    private String contenu;
+    private Date date;
+
+
+    public Commentaire() {}
+
+    public Commentaire(Commentaire CommRepondu, Compte auteur, String contenu, Date date)
+    {
+        this.date = date;
+        this.contenu = contenu;
+        this.CommRepondu = CommRepondu;
+        this.auteur = auteur;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getIdCommentaire() {return idCommentaire;}
+    private void setIdCommentaire(long idCommentaire) {this.idCommentaire = idCommentaire;}
+
+    @Column(name = "contenu", nullable = false)
+    public String getContenu() {return contenu;}
+    public void setContenu(String contenu) {this.contenu = contenu;}
+
+    @Column(name = "date", nullable = false)
+    public Date getDate() {return date;}
+    public void setDate(Date date) {this.date = date;}
+
+    @ManyToOne
+    @JoinColumn(name = "id_auteur", nullable = false)
+    public Compte getAuteur() {return auteur;}
+    public void setAuteur(Compte auteur) {this.auteur = auteur;}
+}
+
