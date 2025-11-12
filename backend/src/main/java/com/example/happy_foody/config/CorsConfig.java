6aa1daccd2141daf60f31ceea03754fb0b4953ea
ABellik <admblk04@gmail.com>
2025-11-12 20:51:45 +0100

    1st step of hebergement

--- backend/src/main/java/com/example/happy_foody/config/CorsConfig.java
+++ backend/src/main/java/com/example/happy_foody/config/CorsConfig.java
@@ -1,4 +1,22 @@
 package com.example.happy_foody.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://happyfoody.vercel.app") // à modifier après déploiement du front
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
